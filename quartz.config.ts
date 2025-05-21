3ea0eeae2551fdab74ef3789ba12c1b3e7b82b11
harryob <55142896+harryob@users.noreply.github.com>
2025-05-21 10:00:45 +0100

    correct base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "hry-gh.github.io/dm-ref",
+    baseUrl: "ref.harry.live",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
